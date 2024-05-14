6aad7cda5f257025aec6b2922f4fce29ca48407a
tsa96 <tom@trumpetdust.org>
2024-05-14 11:08:04 +0100

    Custom vite plugin for inlining SVGs

---
+++ plugins/svg-inline.ts
@@ -0,0 +1,74 @@
+import { Plugin } from 'vite';
+import * as mdi from '@mdi/js';
+import * as si from 'simple-icons';
+import * as mom from '../src/assets/images/paths';
+
+const icons = {
+  mdi: mdi,
+  si: {
+    steam: si.siSteam.path,
+    discord: si.siDiscord.path,
+    twitter: si.siTwitter.path,
+    github: si.siGithub.path,
+    youtube: si.siYoutube.path,
+    twitch: si.siTwitch.path,
+    opencollective: si.siOpencollective.path
+  },
+  mom: mom
+};
+
+// Simple SVG inliner script.
+// Example:
+// [[ icon mdi/account ]]
+// [[ icon si/steam ]]
+export default function (): Plugin {
+  const tagMatcher = /\[\[ path (.+) ]]/gi;
+
+  function transformer(html: string) {
+    const matches = html.matchAll(tagMatcher);
+
+    for (const match of matches) {
+      const [tag, src] = match;
+      let [type, name] = src?.split('/') ?? [];
+
+      if (type === 'mdi') {
+        name = camelize(`mdi-${name}`);
+      }
+
+      const path = icons[type]?.[name];
+      if (!path) {
+        throw new Error(
+          `svg-inline: failed to load icon ${src}! Tell Tom if this script seems broken!`
+        );
+      }
+
+      html = html.replace(tag, `<path fill="currentColor" d="${path}"></path>`);
+    }
+
+    return html;
+  }
+
+  return {
+    name: 'svg-inliner',
+    transformIndexHtml: {
+      handler(html) {
+        return transformer(html);
+      }
+    }
+  };
+}
+
+//         _
+//     .--' |
+//    /___^ |     .--.
+//        ) |    /    \
+//       /  |  /`      '.
+//      |   '-'    /     \
+//      \         |      |\
+//       \    /   \      /\|
+//        \  /'----`\   /
+//        |||       \\ |
+//        ((|        ((|
+//        |||        |||
+// jgs   //_(       //_(
+const camelize = (s) => s.replace(/-./g, ([_, x]) => x.toUpperCase());
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite';
+import svgInline from './plugins/svg-inline';
+
 export default defineConfig({
-  root: 'src'
+  root: 'src',
+  plugins: [svgInline()]
 });
