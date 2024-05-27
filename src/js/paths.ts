86930b37fb8d84574002617d8a5e876767390024
tsa96 <tom@trumpetdust.org>
2024-05-27 01:36:53 +0100

    Run Prettier format

--- src/js/paths.ts
+++ src/js/paths.ts
@@ -1,2 +1,3 @@
-export const logo = 'm4.1 12 4-4 4 4-4 4-4-4zm6.3-10H3.6l10 10-10 10h6.8l10-10-10-10z';
+export const logo =
+  'm4.1 12 4-4 4 4-4 4-4-4zm6.3-10H3.6l10 10-10 10h6.8l10-10-10-10z';
 export const arrowRight = 'M10.4 2 3.6 2 13.6 12 3.6 22 10.4 22 20.4 12z';
