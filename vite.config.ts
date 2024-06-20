64076c6d2cf0bcc5a6f1f4ec9ad5fe8c53e5b7f9
Tom Ashford <tom@trumpetdust.org>
2024-06-20 21:09:21 +0100

    Mobile styles progress (#14)
    * Basic mobile styles
    
    * chore: Update mobile styles for better responsiveness
    
    * Removes useless mobile breakpoint variables
    
    * Add postcss-custom-media
    
    * Use @custom-media queries in css
    
    ---------
    
    Co-authored-by: lodyb <lodyb>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import injectHTML from './plugins/html-inject';
 import svgInline from './plugins/svg-inline';
+import postcssCustomMedia from 'postcss-custom-media';
 
 export default defineConfig({
   root: 'src',
@@ -11,5 +12,10 @@ export default defineConfig({
     emptyOutDir: true,
     target: 'es2020' // Can up this in future if needed
   },
-  plugins: [svgInline(), injectHTML()]
+  plugins: [svgInline(), injectHTML()],
+  css: {
+    postcss: {
+      plugins: [postcssCustomMedia]
+    }
+  }
 });
